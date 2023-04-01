b804e6bbe142d4697732cd701d3d57865ac1be21
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-04-01 04:28:30 +0200

    more itrations

--- main.py
+++ main.py
@@ -30,20 +30,21 @@ def zero_fill(file_path):
     FileNotFoundError: If the file is not found.
     PermissionError: If there is no permission to delete the file.
   """
-  try:
-    with open(file_path, "ba+", buffering=0) as f:
-      length = f.tell() # Get the file's length in bytes
-      f.seek(0)
-      f.write(bytearray(length)) # Overwrites the file's content with zeros. b'\x00\x00\x00\x00'
-  except FileNotFoundError as e:
-    print(f"Error: File not found - {file_path}")
-    return
-  except PermissionError as e:
-    print(f"Error: Permission denied - {file_path}")
-    return
-  except Exception as e:
-    print(f"Error: {e}")
-    return
+  for _ in range(2):
+    try:
+      with open(file_path, "ba+", buffering=0) as f:
+        length = f.tell() # Get the file's length in bytes
+        f.seek(0)
+        f.write(bytearray(length)) # Overwrites the file's content with zeros. b'\x00\x00\x00'
+    except FileNotFoundError as e:
+      print(f"Error: File not found - {file_path}")
+      return
+    except PermissionError as e:
+      print(f"Error: Permission denied - {file_path}")
+      return
+    except Exception as e:
+      print(f"Error: {e}")
+      return
 
   os.remove(file_path)
 
@@ -64,39 +65,40 @@ def dod_5220_22_m(file_path):
     FileNotFoundError: If the file is not found.
     PermissionError: If there is no permission to delete the file.
   """
-  try:
-    with open(file_path, "ba+", buffering=0) as f:
-      length = f.tell() # Get the file's length in bytes
+  for _ in range(2):
+    try:
+      with open(file_path, "ba+", buffering=0) as f:
+        length = f.tell() # Get the file's length in bytes
 
-      # Pass 1: Overwrite with zeros
-      f.seek(0)
-      f.write(bytearray(length))
+        # Pass 1: Overwrite with zeros
+        f.seek(0)
+        f.write(bytearray(length))
 
-      # Pass 2: Overwrite with ones
-      f.seek(0)
-      f.write(bytearray([0xFF] * length))
+        # Pass 2: Overwrite with ones
+        f.seek(0)
+        f.write(bytearray([0xFF] * length))
 
-      # Pass 3: Overwrite with random data
-      f.seek(0)
-      random_data = bytearray(random.getrandbits(8) for _ in range(length)) # Generate random bytes
-      f.write(random_data)
+        # Pass 3: Overwrite with random data
+        f.seek(0)
+        random_data = bytearray(random.getrandbits(8) for _ in range(length)) # Generate random bytes
+        f.write(random_data)
 
-      # Verify the final overwrite
-      f.seek(0)
-      file_data = f.read()
-      if file_data != random_data:
-        print(f"Error: Verification failed - {file_path}")
-        return
+        # Verify the final overwrite
+        f.seek(0)
+        file_data = f.read()
+        if file_data != random_data:
+          print(f"Error: Verification failed - {file_path}")
+          return
 
-  except FileNotFoundError as e:
-    print(f"Error: File not found - {file_path}")
-    return
-  except PermissionError as e:
-    print(f"Error: Permission denied - {file_path}")
-    return
-  except Exception as e:
-    print(f"Error: {e}")
-    return
+    except FileNotFoundError as e:
+      print(f"Error: File not found - {file_path}")
+      return
+    except PermissionError as e:
+      print(f"Error: Permission denied - {file_path}")
+      return
+    except Exception as e:
+      print(f"Error: {e}")
+      return
 
   os.remove(file_path)
 
