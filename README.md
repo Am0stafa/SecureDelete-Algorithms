bf086e65a0259da3fc05494d7dbb28a4570ab0ce
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-08-09 21:29:58 +0300

    random_data algo and docs

--- README.md
+++ README.md
@@ -1,1 +1,60 @@
 # SecureDelete-Algorithms
+
+This is a collection of secure delete algorithms that can be used to overwrite data in a way that makes it difficult or impossible to recover. The algorithms included in this collection are:
+
+- [Zero-fill](#1-zero-fill)
+- [DoD 5220.22-M](#2-dod-522022-m)
+- [Random data](#3-random-data)
+
+## 1) Zero-fill
+
+The zero-fill algorithm overwrites the data with zeros, making it difficult to recover. This method involves overwriting the data with a single pass of zeros. While it's the fastest method, it may not be the most secure, as some advanced data recovery techniques might still recover parts of the original data.
+
+## 2) DoD 5220.22-M
+
+The DoD 5220.22-M algorithm overwrites the data with a specific pattern of values, making it very difficult to recover. This method involves three passes:
+
+1. Overwrite with a fixed value (e.g., all zeros or all ones).
+2. Overwrite with the complement of the fixed value used in Pass 1 (e.g., all ones or all zeros).
+3. Overwrite with random data and verify the final overwrite.
+
+This method is more secure than the zero-fill method, but it's also slower and requires more resources.
+
+## 3) Random data
+
+The random data algorithm overwrites the data with random values, making it more difficult to recover. This method involves overwriting the data with random bytes. While it's the most secure method, it's also the slowest, as it requires generating random bytes and writing them to the file.
+
+## Which algorithm is more secure?
+The most secure algorithm is the random data algorithm, as it overwrites the data with completely random values. However, this method is also the slowest and requires more resources. The DoD 5220.22-M algorithm is also very secure and is a good compromise between security and speed. The zero-fill algorithm is the fastest, but it may not be as secure as the other methods.
+
+
+
+## How data is deleted on a hard disk
+
+When a file is deleted on a hard disk, the operating system marks the space occupied by the file as available for use. However, the data is not actually erased from the disk until it is overwritten by new data. This means that it's possible to recover deleted files using specialized data recovery software.
+<br>
+Secure delete algorithms work by overwriting the data with new data, making it more difficult or impossible to recover. On a hard disk, this involves overwriting the data with new values multiple times. The number of times the data is overwritten depends on the algorithm used. The DoD 5220.22-M algorithm, for example, overwrites the data three times.
+
+<br>
+It's important to note that secure delete algorithms are not foolproof and may not be able to completely erase all traces of the original data. Advanced data recovery techniques may still be able to recover some or all of the original data. However, using a secure delete algorithm can make it much more difficult to recover the data and can provide an additional layer of security.
+<br>
+Data is not deleted its just overwritten
+
+## Why is it not useful to use on an SSD?
+Solid-state drives (SSDs) use a different technology than hard disk drives (HDDs) to store data. SSDs use flash memory, which has a limited number of write cycles. Overwriting data on an SSD can cause unnecessary wear on the flash memory and reduce the lifespan of the drive. Additionally, SSDs use wear-leveling algorithms that can make it difficult to securely delete data. For these reasons, it's generally not recommended to use secure delete algorithms on SSDs.
+
+
+
+
+### Usage
+
+To use these algorithms, you can either implement them in your own code or use a tool that already implements them. Some examples of tools that use these algorithms are:
+
+- [Secure File Deletion Tool](https://github.com/username/secure-delete-tool)
+- [Eraser](https://eraser.heidi.ie/)
+
+### Contributing
+
+Contributions to this collection are welcome! If you have a secure delete algorithm that you'd like to add, please submit a pull request.
+
+
--- main.py
+++ main.py
@@ -2,12 +2,15 @@ import os
 import argparse
 import random
 
+'''To delete a file or directory first move this script to the directory where the file or directory is located and then run the script with the -f or -d option and the file or directory name as an argument. You can also specify the overwrite method with the -m option. The default method is zero-fill. You can also use dod_5220_22_m or random_data. The script will delete the file or directory and overwrite the data with the specified method.'''
+
+'''There are several secure delete algorithms that can be used to overwrite data in a way that makes it difficult or impossible to recover.'''
+
 
 argparser = argparse.ArgumentParser(description="Secure file deletion tool")
 argparser.add_argument("-f", "--file", help="File to delete")
 argparser.add_argument("-d", "--directory", help="Directory to delete")
-argparser.add_argument("-n", "--passes", type=int, default=3, help="Number of overwrite passes (default: 3)")
-# argparser.add_argument("-m", "--method", default="zero-fill", help="Overwrite method (default: zero-fill)")
+argparser.add_argument("-m", "--method", default="zero-fill", help="Overwrite method (default: zero-fill) you can also use dod_5220_22_m or random_data")
 args = argparser.parse_args()
 
 
@@ -30,24 +33,22 @@ def zero_fill(file_path):
     FileNotFoundError: If the file is not found.
     PermissionError: If there is no permission to delete the file.
   """
-  for _ in range(2):
-    try:
-      with open(file_path, "ba+", buffering=0) as f:
-        length = f.tell() # Get the file's length in bytes
-        f.seek(0)
-        f.write(bytearray(length)) # Overwrites the file's content with zeros. b'\x00\x00\x00'
-    except FileNotFoundError as e:
-      print(f"Error: File not found - {file_path}")
-      return
-    except PermissionError as e:
-      print(f"Error: Permission denied - {file_path}")
-      return
-    except Exception as e:
-      print(f"Error: {e}")
-      return
+  try:
+    with open(file_path, "ba+", buffering=0) as f:
+      length = f.tell() # Get the file's length in bytes
+      f.seek(0)
+      f.write(bytearray(length)) # Overwrites the file's content with zeros. b'\x00\x00\x00'
+  except FileNotFoundError as e:
+    print(f"Error: File not found - {file_path}")
+    return
+  except PermissionError as e:
+    print(f"Error: Permission denied - {file_path}")
+    return
+  except Exception as e:
+    print(f"Error: {e}")
+    return
 
   os.remove(file_path)
-
 
 def dod_5220_22_m(file_path):
   """
@@ -65,40 +66,104 @@ def dod_5220_22_m(file_path):
     FileNotFoundError: If the file is not found.
     PermissionError: If there is no permission to delete the file.
   """
-  for _ in range(2):
-    try:
-      with open(file_path, "ba+", buffering=0) as f:
-        length = f.tell() # Get the file's length in bytes
 
-        # Pass 1: Overwrite with zeros
-        f.seek(0)
-        f.write(bytearray(length))
+  try:
+    with open(file_path, "ba+", buffering=0) as f:
+      length = f.tell() # Get the file's length in bytes
 
-        # Pass 2: Overwrite with ones
-        f.seek(0)
-        f.write(bytearray([0xFF] * length))
+      # Pass 1: Overwrite with zeros
+      f.seek(0)
+      f.write(bytearray(length))
 
-        # Pass 3: Overwrite with random data
-        f.seek(0)
-        random_data = bytearray(random.getrandbits(8) for _ in range(length)) # Generate random bytes
-        f.write(random_data)
+      # Pass 2: Overwrite with ones
+      f.seek(0)
+      f.write(bytearray([0xFF] * length))
 
-        # Verify the final overwrite
-        f.seek(0)
-        file_data = f.read()
-        if file_data != random_data:
-          print(f"Error: Verification failed - {file_path}")
-          return
+      # Pass 3: Overwrite with random data
+      f.seek(0)
+      random_data = bytearray(random.getrandbits(8) for _ in range(length)) # Generate random bytes
+      f.write(random_data)
 
-    except FileNotFoundError as e:
-      print(f"Error: File not found - {file_path}")
-      return
-    except PermissionError as e:
-      print(f"Error: Permission denied - {file_path}")
-      return
-    except Exception as e:
-      print(f"Error: {e}")
-      return
+      # Verify the final overwrite
+      f.seek(0)
+      file_data = f.read()
+      if file_data != random_data:
+        print(f"Error: Verification failed - {file_path}")
+        return
+
+  except FileNotFoundError as e:
+    print(f"Error: File not found - {file_path}")
+    return
+  except PermissionError as e:
+    print(f"Error: Permission denied - {file_path}")
+    return
+  except Exception as e:
+    print(f"Error: {e}")
+    return
 
   os.remove(file_path)
 
+def random_data(file_path):
+  """
+  Securely delete a file using the random data method.
+
+  This method involves overwriting the data with random bytes.
+  While it's the most secure method, it's also the slowest, as it
+  requires generating random bytes and writing them to the file.
+  The function opens the file in binary append mode ("ba+")
+  with buffering=0 to prevent any caching of data. It then gets the file's
+  length, seeks back to the beginning, and writes random bytes over the entire file.
+  After the overwrite is complete, the file is deleted
+
+  Args:
+    file_path (str): The path to the file to securely delete.
+
+  Raises:
+    FileNotFoundError: If the file is not found.
+    PermissionError: If there is no permission to delete the file.
+  """
+  try:
+    with open(file_path, "ba+", buffering=0) as f:
+      length = f.tell() # Get the file's length in bytes
+      f.seek(0)
+      random_data = bytearray(random.getrandbits(8) for _ in range(length)) # Generate random bytes
+      f.write(random_data)
+  except FileNotFoundError as e:
+    print(f"Error: File not found - {file_path}")
+    return
+  except PermissionError as e:
+    print(f"Error: Permission denied - {file_path}")
+    return
+  except Exception as e:
+    print(f"Error: {e}")
+    return
+
+  os.remove(file_path)
+
+if __name__ == "__main__":
+  if args.file:
+    if args.method == "zero-fill":
+      zero_fill(args.file)
+    elif args.method == "dod_5220_22_m":
+      dod_5220_22_m(args.file)
+    elif args.method == "random_data":
+      random_data(args.file)
+    else:
+      print(f"Error: Invalid method - {args.method}")
+  elif args.directory:
+    if args.method == "zero-fill":
+      for root, dirs, files in os.walk(args.directory):
+        for file in files:
+          zero_fill(os.path.join(root, file))
+    elif args.method == "dod_5220_22_m":
+      for root, dirs, files in os.walk(args.directory):
+        for file in files:
+          dod_5220_22_m(os.path.join(root, file))
+    elif args.method == "random_data":
+      for root, dirs, files in os.walk(args.directory):
+        for file in files:
+          random_data(os.path.join(root, file))
+    else:
+      print(f"Error: Invalid method - {args.method}")
+  else:
+    print("Error: No file or directory specified")
